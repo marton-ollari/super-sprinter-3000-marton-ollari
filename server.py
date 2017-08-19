f265b295ed357034f2ab887eb9dc8d0f10e90d7d
Ollári Márton <ollarimarton@gmail.com>
2017-08-19 16:21:55 +0200

    form

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/list')
 def index():
-    return render_template('list.html')
+    table = csv_to_list()
+    return render_template('list.html', table=table)
 
 
 @app.route('/story')
@@ -13,5 +14,26 @@ def index():
 def form(story_id=None):
     return render_template('form.html', story_id=story_id)
 
+
+@app.route('/save-table', methods=['POST'])
+def route_save():
+    session['note'] = request.form['note']
+    return redirect('/')
+
+
+def csv_to_list():
+    with open('table.csv', "r") as file:
+        lines = file.readlines()
+    table = [element.replace("\n", "").split(";") for element in lines]
+    return table
+
+
+def list_to_csv(table):
+    with open('table.csv', "w") as file:
+        for record in table:
+            row = ';'.join(record)
+            file.write(row + "\n")
+
 if __name__ == '__main__':
+    app.secret_key = 'secret'
     app.run(debug=True, port=5000)
